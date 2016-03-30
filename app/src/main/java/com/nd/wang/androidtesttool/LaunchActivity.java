9dc16ea4a3200eb030e73fa4785872045fba8d27
wang <183181731@qq.com>
2016-03-30 10:19:59 +0800

    add service status display

--- app/src/main/java/com/nd/wang/androidtesttool/LaunchActivity.java
+++ app/src/main/java/com/nd/wang/androidtesttool/LaunchActivity.java
@@ -1,6 +1,10 @@
 package com.nd.wang.androidtesttool;
 
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,7 +12,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LaunchActivity extends AppCompatActivity {
     final String TAG = "TestTools";
@@ -17,6 +25,9 @@ public class LaunchActivity extends AppCompatActivity {
     Button rebootButton;
     Button cleanButton;
     EditText timeText;
+    TextView textStatus;
+    Timer timer;
+    Handler handler;
     long intervalTime = 1800000;
 
     @Override
@@ -24,6 +35,8 @@ public class LaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
         timeText = (EditText) findViewById(R.id.timeText);
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        handler = new MyHandler();
 
         startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +53,7 @@ public class LaunchActivity extends AppCompatActivity {
                 intent.putExtra("time", intervalTime);
                 startService(intent);
                 Toast.makeText(getApplicationContext(), "start DDMS Service", Toast.LENGTH_LONG).show();
+                isServiceRunning();
             }
         });
 
@@ -49,7 +63,7 @@ public class LaunchActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DDMSService.class);
                 stopService(intent);
-                Toast.makeText(getApplicationContext(), "stop DDMS Service", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "stop DDMS Service", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -62,8 +76,6 @@ public class LaunchActivity extends AppCompatActivity {
             }
         });
 
-
-
         cleanButton = (Button) findViewById(R.id.cleanButton);
         cleanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,5 +86,51 @@ public class LaunchActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "clean log complete", Toast.LENGTH_LONG).show();
             }
         });
+        new Thread(){
+            public void run(){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TimerTask task = new TimerTask() {
+                            @Override
+                            public void run() {
+                                handler.sendEmptyMessage(1);
+                            }
+                        };
+                        timer = new Timer(true);
+                        timer.schedule(task, 1000, 1000);
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private final class MyHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    if (isServiceRunning()){
+                        textStatus.setText("Service is Running");
+                        textStatus.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorRunning));
+                    }else {
+                        textStatus.setText("Service is Stop");
+                        textStatus.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.colorStop));
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private boolean isServiceRunning(){
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+            if ("com.nd.wang.androidtesttool.DDMSService".equals(service.service.getClassName())){
+                return true;
+            }
+        }
+        return false;
     }
 }
